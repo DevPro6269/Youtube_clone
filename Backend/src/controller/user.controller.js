83cb0d640af3c7dce8bb326ff080c8c6f3172c2e
DevPro6269 <devrathore653@gmail.com>
2025-02-13 11:28:08 +0530

    implemented users controller

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -1,109 +1,192 @@
 import sendEmail from "../config/sendGrid.config.js";
 import User from "../model/user.model.js";
 import ApiError from "../utils/apiError.js";
-import ApiResponse from "../utils/apiResponse.js"
-import generateAccessToken from "../utils/genrateAccessToken.js"
-
+import ApiResponse from "../utils/apiResponse.js";
+import generateAccessToken from "../utils/genrateAccessToken.js";
 
 const otpStore = {};
 
-export async function SignUp(req,res){
-   const{firstName,lastName,password,email}=req.body;
-   if(!firstName||!lastName||!password||!email)return res.status(400).json(new ApiError(400,"all fields must be required"))
+export async function SignUp(req, res) {
+  const { firstName, lastName, password, email } = req.body;
+  if (!firstName || !lastName || !password || !email)
+    return res
+      .status(400)
+      .json(new ApiError(400, "all fields must be required"));
 
-   const user = await User.findOne({email});
+  const user = await User.findOne({ email });
 
-   if(user)return res.status(400).json(new ApiError(400,"user is already registered with this email"))
-       
-    try {
-        const { otp, otpExpiration } = await sendEmail(email);
-    
-        // Store OTP and expiration time temporarily (could be in memory or session)
-        otpStore[email] = { otp, otpExpiration };
-    
-        // Respond to the user to inform them that OTP was sent
-        return res.status(200).json({
-          message: 'OTP has been sent to your email for verification',
-        });
-      } catch (error) {
-        console.log(error)
-        return res.status(500).json(new ApiError(500, 'Error sending OTP'));
-      }
-            
+  if (user)
+    return res
+      .status(400)
+      .json(new ApiError(400, "user is already registered with this email"));
 
-    // const newUser = await User.create({
-    //     firstName,
-    //     lastName,
-    //     email,
-    //     password
-    // })
+  try {
+    const { otp, otpExpiration } = await sendEmail(email);
 
-    // if(!newUser)return res.status(400).json(400,"error while creating a new user")
+    // Store OTP and expiration time temporarily (could be in memory or session)
+    otpStore[email] = { otp, otpExpiration };
 
-    //     const token = generateAccessToken(newUser._id); // Use _id as userId
+    // Respond to the user to inform them that OTP was sent
+    return res.status(200).json({
+      message: "OTP has been sent to your email for verification",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(new ApiError(500, "Error sending OTP"));
+  }
 
-    //     // Send the token in an HTTP-only cookie
-    //     res.cookie("accessToken", token, {
-    //         httpOnly: true,
-    //         maxAge: 3600000, // Token expiration time in ms (1 hour)
-    //         secure: process.env.NODE_ENV === 'production', // Use secure cookie in production
-    //         sameSite: 'Strict', // Prevent CSRF attacks
-    //     });
+  // const newUser = await User.create({
+  //     firstName,
+  //     lastName,
+  //     email,
+  //     password
+  // })
 
+  // if(!newUser)return res.status(400).json(400,"error while creating a new user")
 
-    //  return res.status(201).json(new ApiResponse(201,newUser,"user sign up successfully"))
+  //     const token = generateAccessToken(newUser._id); // Use _id as userId
 
+  //     // Send the token in an HTTP-only cookie
+  //     res.cookie("accessToken", token, {
+  //         httpOnly: true,
+  //         maxAge: 3600000, // Token expiration time in ms (1 hour)
+  //         secure: process.env.NODE_ENV === 'production', // Use secure cookie in production
+  //         sameSite: 'Strict', // Prevent CSRF attacks
+  //     });
+
+  //  return res.status(201).json(new ApiResponse(201,newUser,"user sign up successfully"))
 }
 
-export async function verifyOtp(req,res){
-    const { email, otp } = req.body;
-    console.log(req.body);
+export async function verifyOtp(req, res) {
+  const { email, otp } = req.body;
+  console.log(req.body);
+
+  if (!email || !otp) {
+    return res
+      .status(400)
+      .json(new ApiError(400, "Email and OTP are required"));
+  }
+
+  if (!otpStore[email]) {
+    return res.status(400).json(new ApiError(400, "OTP not found or expired"));
+  }
+
+  const { otp: storedOtp, otpExpiration } = otpStore[email];
+
+  if (otpExpiration < Date.now()) {
+    return res.status(400).json(new ApiError(400, "OTP has expired"));
+  }
+
+  if (storedOtp != otp) {
+    return res.status(400).json(new ApiError(400, "Invalid OTP"));
+  }
+
+  try {
+    const newUser = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email,
+      password: req.body.password, // Make sure you hash the password before saving
+      isVerified: true, // Mark as verified
+    });
+
+    const token = generateAccessToken(newUser._id); // Use _id as userId
+
+    // Send the token in an HTTP-only cookie
+    res.cookie("accessToken", token, {
+      httpOnly: true,
+      maxAge: 3600000, // Token expiration time in ms (1 hour)
+      secure: process.env.NODE_ENV === "production", // Use secure cookie in production
+      sameSite: "Strict", // Prevent CSRF attacks
+    });
+
+    delete otpStore[email];
+    return res.status(201).json({
+      message: "User created successfully and verified",
+      user: newUser,
+    });
+  } catch (error) {
+    return res.status(500).json(new ApiError(500, "Error creating user"));
+  }
+}
+
+export async function login(req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res
+      .status(400)
+      .json(new ApiError(400, "email and password field is required"));
+
+  const user = await User.findOne({ email });
+  if (!user)
+    return res
+      .status(404)
+      .json(new ApiError(404, "user is not registered with this email id"));
+
+  const isValidPassword = user.comparePassword(password);
+  if (!isValidPassword)
+    return res.status(400).json(new ApiError(400, "user password is wrong"));
+
+  const token = generateAccessToken(user._id);
+
+  res.cookie("accessToken", token, {
+    httpOnly: true,
+    secure: false,
+    maxAge: 3600000,
+    sameSite: "Strict",
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "user login succesfully"));
+}
+
+export async function logout(req, res) {
+  const { user } = req;
+  if (!user)
+    return res
+      .status(404)
+      .json(new ApiError(404, "no user find please login and try again"));
+
+      res.clearCookie("accessToken", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Only set secure: true for production environment
+        sameSite: 'strict', // Added sameSite for additional security
+      });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "user logout successfully"));
+}
+
+export async function profile(req,res) {
+    const{user}=req;
+    if(!user)return res.status(404).json(new ApiError(404,"user not found please login and try again"));
     
-    if (!email || !otp) {
-        return res.status(400).json(new ApiError(400, 'Email and OTP are required'));
-      }
+    return res.status(200).json(new ApiResponse(200,user,"user profile retrived successfully"))
+}
 
-      if (!otpStore[email]) {
-        return res.status(400).json(new ApiError(400, 'OTP not found or expired'));
-      }
-       
-      const { otp: storedOtp, otpExpiration } = otpStore[email];
-       
-      if (otpExpiration < Date.now()) {
-        return res.status(400).json(new ApiError(400, 'OTP has expired'));
-      }
-      
-      if (storedOtp != otp) {
-        return res.status(400).json(new ApiError(400, 'Invalid OTP'));
-      }
+export async function updateProfile(req,res) {
+    const{user}=req;
 
-      try {
-        const newUser = await User.create({
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          email,
-          password: req.body.password,  // Make sure you hash the password before saving
-          isVerified: true,  // Mark as verified
-        });
-        
-        
-        const token = generateAccessToken(newUser._id); // Use _id as userId
+    if(!user)return res.status(404).json(new ApiError(404,"user not found please login and try again"));
 
-        // Send the token in an HTTP-only cookie
-        res.cookie("accessToken", token, {
-            httpOnly: true,
-            maxAge: 3600000, // Token expiration time in ms (1 hour)
-            secure: process.env.NODE_ENV === 'production', // Use secure cookie in production
-            sameSite: 'Strict', // Prevent CSRF attacks
-        });
+    const{firstName,lastName}= req.body
+    
+    if (!firstName && !lastName) {
+        return res.status(400).json(new ApiError(400, "At least one of 'firstName' or 'lastName' should be provided."));
+     }
 
-
-        delete otpStore[email];
-        return res.status(201).json({
-            message: 'User created successfully and verified',
-            user: newUser,
-          });
-        } catch (error) {
-          return res.status(500).json(new ApiError(500, 'Error creating user'));
-        }
+    if(firstName){
+        user.firstName=firstName
+    }
+    if(lastName){
+        user.lastName=lastName
+    }
+   
+    const updatedUser = await user.save()
+   if(!updatedUser)return res.status(500).json(new ApiError(500,"something went wrong"))
+ updatedUser.password=undefined
+    return res.status(200).json(new ApiResponse(200,updatedUser,"user details updated"))
 }
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,8 +1,14 @@
 import express from "express"
-import { SignUp } from "../controller/user.controller.js";
+import { login, logout, profile, SignUp, updateProfile } from "../controller/user.controller.js";
 import WrapAsync from "../utils/WrapAsync.js"
-
+import isAuthenticate from "../middlewares/auth.middleware.js"
 const router = express.Router();
 
 router.route("/signup").post(WrapAsync(SignUp))
+router.route("/login").post(WrapAsync(login))
+
+// protected routes
+router.route("/logout").post(isAuthenticate,WrapAsync(logout))
+router.route("/profile").get(isAuthenticate,WrapAsync(profile))
+.post(isAuthenticate,WrapAsync(updateProfile))
 export default router;
